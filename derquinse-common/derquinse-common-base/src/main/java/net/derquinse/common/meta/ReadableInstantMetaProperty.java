9cd721c8214b8b46bd70b3014f7da5b234c92a15
andresrc@gmail.com <andresrc@gmail.com@95e9dd4e-c79d-11de-a9c8-2fbcfba63733>
2012-01-20 22:55:13 +0000

    Version 1.0.16
    git-svn-id: https://derquinse-commons.googlecode.com/svn/trunk@392 95e9dd4e-c79d-11de-a9c8-2fbcfba63733

---
+++ derquinse-common/derquinse-common-base/src/main/java/net/derquinse/common/meta/ReadableInstantMetaProperty.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.derquinse.common.meta;
+
+import javax.annotation.Nullable;
+
+import org.joda.time.ReadableInstant;
+
+import com.google.common.base.Predicate;
+
+/**
+ * Class for string-valued property descriptor.
+ * @author Andres Rodriguez
+ * @param <C> Containing type.
+ */
+public abstract class ReadableInstantMetaProperty<C> extends ComparableMetaProperty<C, ReadableInstant> {
+	/**
+	 * Default constructor.
+	 * @param name Property name.
+	 * @param required True if the property is required.
+	 * @param validity Validity predicate.
+	 * @param defaultValue Default value for the property.
+	 */
+	protected ReadableInstantMetaProperty(String name, boolean required,
+			@Nullable Predicate<? super ReadableInstant> validity, @Nullable ReadableInstant defaultValue) {
+		super(name, required, validity, defaultValue);
+	}
+
+	/**
+	 * Constructor.
+	 * @param name Property name.
+	 * @param required True if the property is required.
+	 * @param validity Validity predicate.
+	 */
+	protected ReadableInstantMetaProperty(String name, boolean required,
+			@Nullable Predicate<? super ReadableInstant> validity) {
+		super(name, required, validity);
+	}
+
+	/**
+	 * Constructor.
+	 * @param name Property name.
+	 * @param required True if the property is required.
+	 */
+	protected ReadableInstantMetaProperty(String name, boolean required) {
+		super(name, required);
+	}
+
+}
